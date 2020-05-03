74ea6ca0d0c72f7081ab8f1cc33f0c99f7b6779e
LeeAYoung99 <37390008+LeeAYoung99@users.noreply.github.com>
2020-05-03 19:09:02 +0900

    버튼이미지수정
    버튼이미지수정 및 오브젝트 조정

---
+++ Assets/Script/DontClick.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DontClick : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (!EventSystem.current.IsPointerOverGameObject())
+            {
+                //클릭 처리
+            }
+
+        }
+    }
+}
